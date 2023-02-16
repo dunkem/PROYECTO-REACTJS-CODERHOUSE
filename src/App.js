f07c7c23ca0dc14d546b1d7062f1516069ed6b4a
Douglas Dunke <dunkem55mail.com>
2023-02-16 12:26:12 -0300

    2dapreentrega+dunkem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Product from './components/Product';
 import Navbar from './components/Navbar';
+import Products from './components/Products';
+import { BrowserRouter, Router, Route } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>NOMBRE DE TIENDA</h1>
+      <h1>Dreamflights</h1>
       <Navbar/>
+      <Products/>
       <Product/>
     </div>
   );
---
+++ src/components/Product-data.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: 1
+        name: "viaje 1",
+        productType: "descripcion",
+        price: 700,
+        image:
+        ""
+        description: "asfsafsafsafsaf"
+
+    },
+    {
+        id: 1
+        name: "viaje 1",
+        productType: "descripcion",
+        price: 700,
+        image:
+        ""
+        description: "asfsafsafsafsaf"
+    },
+];
+
+export default products;
